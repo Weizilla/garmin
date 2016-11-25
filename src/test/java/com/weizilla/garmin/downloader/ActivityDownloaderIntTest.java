bec55d50ffec1136d3ec638fc233b26abc24b4d3
Wei Yang <wei@weizilla.com>
2016-11-25 09:00:04 -0600

    finish integration tests

--- src/test/java/com/weizilla/garmin/downloader/ActivityDownloaderIntTest.java
+++ src/test/java/com/weizilla/garmin/downloader/ActivityDownloaderIntTest.java
@@ -1,6 +1,8 @@
 package com.weizilla.garmin.downloader;
 
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.google.common.io.Resources;
+import com.weizilla.garmin.entity.Activity;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +11,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,5 +37,33 @@ public class ActivityDownloaderIntTest
     public void downloaderIsInitializedCorrectlyBySpring() throws Exception
     {
         assertThat(downloader).isNotNull();
+    }
+
+    @Test
+    public void downloadsActivitiesWithUrlCalls() throws Exception
+    {
+        stubFor(get(urlMatching("/sso/login.*"))
+            .willReturn(aResponse()
+                .withHeader("Content-Type", "text/html; charset=utf-8")
+                .withBody(readResource("int-test/lt-lookup-response.html"))));
+        stubFor(post(urlMatching("/sso/login.*"))
+            .willReturn(aResponse()
+                .withHeader("Content-Type", "text/html; charset=utf-8")
+                .withBody(readResource("int-test/login-response.html"))));
+        stubFor(get(urlMatching("/post-auth/login.*"))
+            .willReturn(aResponse()
+                .withHeader("location", "http://localhost:8080/redirect")));
+        stubFor(get(urlMatching("/proxy/activity-search-service-1.2.*"))
+            .willReturn(aResponse()
+                .withHeader("Content-Type", "application/json")
+                .withBody(readResource("int-test/get-activities-response.json"))));
+        List<Activity> activities = downloader.download();
+        assertThat(activities).isNotEmpty();
+    }
+
+    private static String readResource(String filename) throws Exception
+    {
+        URL url = Resources.getResource(filename);
+        return Resources.toString(url, StandardCharsets.UTF_8);
     }
 }
