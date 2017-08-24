387a9f2295c23b39736a839aec83b7ea4cb6d44b
Wei Yang <wei@weizilla.com>
2017-08-23 21:18:43 -0500

    Add derived date for Activity

--- garmin-lib/src/main/java/com/weizilla/garmin/entity/Activity.java
+++ garmin-lib/src/main/java/com/weizilla/garmin/entity/Activity.java
@@ -2,11 +2,13 @@ package com.weizilla.garmin.entity;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.immutables.value.Value.Derived;
 import org.immutables.value.Value.Immutable;
 
 import javax.measure.Quantity;
 import javax.measure.quantity.Length;
 import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Immutable
@@ -18,6 +20,11 @@ public interface Activity {
     String getType();
 
     LocalDateTime getStart();
+
+    @Derived
+    default LocalDate getDate() {
+        return getStart().toLocalDate();
+    }
 
     Duration getDuration();
 
